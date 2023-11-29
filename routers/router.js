eea46a9b86fd15a1c56e958eea0eef1fd6d57874
Erdmann Rémy <r.erdmann@stag.technofuturtic.education>
2023-11-29 14:29:39 +0100

    ProjetBack-endExpress/29-11/V1

--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,7 @@
 const authRouter = require('./auth.router');
 const characterRouter = require('./character.router');
 const movieRouter = require('./movie.router');
+const articleRouter = require('/article.router');
 
 
 
@@ -9,5 +10,6 @@ const router = require('express').Router();
 router.use('/character', characterRouter);
 router.use('/movie', movieRouter);
 router.use('/auth', authRouter);
+router.use('/article',articleRouter);
 
 module.exports = router;
